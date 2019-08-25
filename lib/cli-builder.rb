d79f83b4b497730a642d968dd2facb391a596282
burnt43 <jms.crsn@gmail.com>
2019-08-24 21:13:15 -0400

    adding complexity

--- lib/cli-builder.rb
+++ lib/cli-builder.rb
@@ -1,11 +1,36 @@
 require 'hashie'
 
 module CliBuilder
-  module Parse
-    Options = Class.new(Hashie::Mash)
-    Result = Struct.new(:command, :options)
-    Error  = Struct.new(:type, :message) do
-      # 1 UNKNOWN_COMMAND
+  module Syntax
+    class Parser
+      def initialize(syntax_string)
+        @syntax_string = syntax_string
+      end
+
+      def parse
+        return Data.new unless @syntax_string
+
+        parse_state = :looking_for_arguments
+
+        @syntax_string.each_char do |c|
+          puts c
+        end
+      end
+    end
+
+    class Data
+      def initialize
+      end
+    end
+  end
+
+  module Input
+    module Parse
+      Options = Class.new(Hashie::Mash)
+      Result = Struct.new(:command, :options)
+      Error  = Struct.new(:type, :message) do
+        # 1 UNKNOWN_COMMAND
+      end
     end
   end
 
@@ -22,26 +47,28 @@ module CliBuilder
     end
 
     def register_command(command_name, syntax_string = nil, &block)
+      CliBuilder::Syntax::Parser.new(syntax_string).parse
       @commands[command_name.to_sym] = CommandData.new(syntax_string, block)
     end
 
     def parse_input(input_string)
-      match_data = /\A(\w+) (.*)\z/.match(input_string)
+      match_data = /\A(\w+)( (.*))?\z/.match(input_string)
 
       return unless match_data
 
       potential_command = match_data.captures[0].to_sym
-      potential_options = match_data.captures[1]
+      potential_options = match_data.captures[2]
+      puts "potential_options: #{potential_options}"
 
       command_data = @commands[potential_command]
 
       unless command_data
-        return Parse::Error.new(1, "unknown command: \"#{potential_command}\"")
+        return Input::Parse::Error.new(1, "unknown command: \"#{potential_command}\"")
       end
 
       if command_data.syntax_string
       else
-        Parse::Result.new(potential_command, Options.new)
+        Input::Parse::Result.new(potential_command, Input::Parse::Options.new)
       end
     end
 
--- test/foobar_test.rb
+++ test/foobar_test.rb
@@ -3,11 +3,20 @@ require './test/test_helper.rb'
 class FoobarTest < Minitest::Test
   def test_foobar
     prompt = CliBuilder::Prompt.new('test >')
+
     prompt.register_command(:command1) do
       # noop
     end
 
+    prompt.register_command(:command2, '<name>') do
+      # noop
+    end
+
     result = prompt.parse_input('command1')
-    assert_instance_of(CliBuilder::Parse::Result, result)
+    assert_instance_of(CliBuilder::Input::Parse::Result, result)
+    assert_equal(:command1, result.command)
+    assert_empty(result.options)
+
+    result = prompt.parse_input('command2 foobar')
   end
 end
